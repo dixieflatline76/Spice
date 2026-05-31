20856fc514a39b4322b89670015681c9af7aca3d
Karl K <karl.kwong@gmail.com>
2026-05-31 13:50:25 +0200

    fix: MSIX wallpaper SetWallpaper 0x80070002 + hide update check in store build

--- config/distribution_test.go
+++ config/distribution_test.go
@@ -9,8 +9,9 @@ import (
 // TestIsStoreDistribution_StandardBuild verifies that IsStoreDistribution()
 // returns false when built without appstore or msstore tags.
 // The "true" path is verified by building with:
-//   go build -tags "release msstore" ./cmd/spice
-//   go build -tags "release appstore" ./cmd/spice
+//
+//	go build -tags "release msstore" ./cmd/spice
+//	go build -tags "release appstore" ./cmd/spice
 func TestIsStoreDistribution_StandardBuild(t *testing.T) {
 	assert.False(t, IsStoreDistribution(), "Standard build should NOT be a store distribution")
 }
